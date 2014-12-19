f3194179cbfe4aa71a8048a02155c3780f0f1242
Jeff Hollister <jeff.w.hollister@gmail.com>
2014-12-19 10:05:20 -0500

    automatic build of site

--- rmd_posts/2015-01-14-03-Clean.R
+++ rmd_posts/2015-01-14-03-Clean.R
@@ -37,23 +37,42 @@ obs3to7
 ## ----more_data_frame_index-----------------------------------------------
 #First, there are a couple of ways to use the column names
 iris$Petal.Length
-iris["Petal.Length"]
+head(iris["Petal.Length"])
 #Multiple colums
-iris[c("Petal.Length","Species")]
+head(iris[c("Petal.Length","Species")])
 #Now we can combine what we have seen to do some more complex queries
 #Lets get all the data for Species with a petal length greater than 6
 big_iris<-iris[iris$Petal.Length>=6,]
-big_iris
+head(big_iris)
 #Or maybe we want just the sepal widths of the virginica species
 virginica_iris<-iris$Sepal.Width[iris$Species=="virginica"]
-virginica_iris
+head(virginica_iris)
 
 
 ## ----subset_examp--------------------------------------------------------
 #And redo what we did above
 big_iris_subset<-subset(iris,subset=Petal.Length>=6)
-big_iris_subset
+head(big_iris_subset)
 virginica_iris_subset<-subset(iris,subset=Species=="virginica",select=Sepal.Width)
-virginica_iris_subset
+head(virginica_iris_subset)
+
+
+## ----cbind_examp---------------------------------------------------------
+#Create a new vector to add to the iris data frame
+categories<-sample(1:3,nrow(iris),replace=T)
+iris_cbind<-cbind(iris,categories)
+head(iris_cbind)
+
+
+## ----transform_examp-----------------------------------------------------
+
+
+## ----rbind_examp---------------------------------------------------------
+#Let's first create a new small example data.frame
+rbind_df<-data.frame(a=1:3,b=c("a","b","c"),c=c(T,T,F),d=rnorm(3))
+#Now an example df to add
+rbind_df2<-data.frame(a=10:12,b=c("x","y","z"),c=c(F,F,F),d=rnorm(3))
+rbind_df<-rbind(rbind_df, rbind_df2)
+rbind_df
 
 
